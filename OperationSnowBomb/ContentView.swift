23a3be943b2ac83a3dc0b6e77a5acfab196f231d
Moyses Azevedo <m.mirazev@gmail.com>
2023-03-31 09:37:38 -0300

    first commit

---
+++ OperationSnowBomb/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  OperationSnowBomb
+//
+//  Created by Moyses Miranda do Vale Azevedo on 31/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OperationSnowBomb/OperationSnowBombApp.swift
@@ -0,0 +1,18 @@
+//
+//  OperationSnowBombApp.swift
+//  OperationSnowBomb
+//
+//  Created by Moyses Miranda do Vale Azevedo on 31/03/23.
+//
+
+import SwiftUI
+
+@main
+struct OperationSnowBombApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
