ede17b8ce77449380a37c6ee36b94180082df211
Moyses Azevedo <m.mirazev@gmail.com>
2023-04-10 11:30:35 -0300

    :sparkles: feat: Add ProjectileProtocol

---
+++ OperationSnowBomb/Utils/Protocols/ProjectileProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  ProjectileProtocol.swift
+//  OperationSnowBomb
+//
+//  Created by Moyses Miranda do Vale Azevedo on 10/04/23.
+//
+
+import Foundation
+
+protocol ProjectileProtocol {
+    func disappear()
+}
