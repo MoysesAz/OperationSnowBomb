969fba7495c24ee28a0399c7bd4737a074bcd4ca
Cicero Nascimento <antoniociceero@gmail.com>
2023-04-11 09:04:10 -0300

    :sparkles: feat: GameBoard and initial configurations

--- OperationSnowBomb/ContentView.swift
+++ OperationSnowBomb/ContentView.swift
@@ -6,16 +6,19 @@
 //
 
 import SwiftUI
+import SpriteKit
 
 struct ContentView: View {
+    var scene: SKScene {
+        let scene = GameBoard()
+        scene.scaleMode = .resizeFill
+        return scene
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { frame in
+            SpriteView(scene: scene)
+                .frame(width: frame.size.width, height: frame.size.height, alignment: .center)
         }
-        .padding()
     }
 }
 
---
+++ OperationSnowBomb/GameBoard.swift
@@ -0,0 +1,15 @@
+//
+//  GameBoard.swift
+//  OperationSnowBomb
+//
+//  Created by Cicero Nascimento on 11/04/23.
+//
+
+import SpriteKit
+
+class GameBoard: SKScene {
+    override func didMove(to view: SKView) {
+        super.didMove(to: view)
+        backgroundColor = .red
+    }
+}
